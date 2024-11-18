fa0f46451fd559ce2532f033a6e4d717c8ef17e3
pyheejin <pyheejin@gmail.com>
2024-11-18 22:38:44 +0900

    장바구니 개수 1초 간격으로 업데이트

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -2,12 +2,16 @@ import 'package:eilly/database/database.dart';
 import 'package:eilly/database/models.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final cartCountProvider = FutureProvider<int>((ref) async {
+final cartCountProvider = StreamProvider<int>((ref) async* {
   final DatabaseHelper db = DatabaseHelper();
   db.initDb();
 
-  final result = await db.getCartCount(1);
-  return result;
+  // 1초 간격으로 데이터베이스에서 장바구니 개수를 확인
+  while (true) {
+    final result = await db.getCartCount(1);
+    yield result;
+    await Future.delayed(const Duration(seconds: 1));
+  }
 });
 
 class CartNotifier extends StateNotifier<List<CartModel>> {
